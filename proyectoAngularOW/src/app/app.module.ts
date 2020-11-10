1ec2ca2f7b4813ea80e7d19f1d647c495d7bb1c8
gonzalopunta <gpuntaperez@safareyes.es>
2020-11-10 16:36:42 +0100

    ejercicio 11 de noviembre completo

--- proyectoAngularOW/src/app/app.module.ts
+++ proyectoAngularOW/src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CorreoComponent } from './Componentes/correo/correo.component';
 import { ListaCorreoComponent } from './Componentes/lista-correo/lista-correo.component';
+import { NuevoCorreoComponent } from './Componentes/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
-    ListaCorreoComponent
+    ListaCorreoComponent,
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
